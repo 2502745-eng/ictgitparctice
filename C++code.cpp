5b19af0dd5b408b0d303c0857f31cedacbe05728
Rehan Zia <rehanzia86@gmail.com>
2025-12-10 12:35:07 +0500

    changes made in C++code in ICTGIT

---
+++ C++code.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Prints "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    
+    // Returns 0 to indicate successful execution
+    return 0;
+}
